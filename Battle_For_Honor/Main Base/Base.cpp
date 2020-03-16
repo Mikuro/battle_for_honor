61cbe0c172530091be40bf2130c862b4d4dd8a62
Daniil <dan.masalikin2017@gmail.com>
2020-03-16 04:47:45 +0300

    fix base

--- Battle_For_Honor/Main Base/Base.cpp
+++ Battle_For_Honor/Main Base/Base.cpp
@@ -1,5 +1,16 @@
 //
-// Created by anton on 02.03.2020.
+//
 //
 
 #include "Base.h"
+
+Base::Base(int x, int y){
+    this->setHealth(1000);
+    this->setArmour("siege");
+    this->setDamage("light");
+    this->setDistance(5);
+    this->setSpeed(0);
+    this->setX(x);
+    this->setY(y);
+    this->setSymbol(222);
+}
